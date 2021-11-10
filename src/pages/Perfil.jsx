657796877d7facdab7656d76f78a06e90ee26f52
paulodetasso <xcloudgameemail@gmail.com>
2021-11-10 15:16:47 -0300

    Perfil feito

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Perfil() {
+  const history = useHistory();
+  const playerEmail = JSON.parse(localStorage.getItem('user')).email;
+
+  const onClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <h1 data-testid="page-title">Perfil</h1>
-    <div>
-      <p>Perfil</p>
-      <Footer />
-    </div>
+    <>
+      <h1 data-testid="page-title">Perfil</h1>
+      <div>
+        <p data-testid="profile-email">{ playerEmail }</p>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ onClick }
+        >
+          Sair
+        </button>
+        <Footer />
+      </div>
+    </>
   );
 }
 
